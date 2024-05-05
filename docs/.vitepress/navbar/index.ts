53ea0449579b9580665cd8ce54a3a64268be994f
heqingpan <heqingpan@126.com>
2024-05-06 00:27:32 +0800

    增加控制台演示链接

--- docs/.vitepress/navbar/index.ts
+++ docs/.vitepress/navbar/index.ts
@@ -2,5 +2,5 @@ import { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.NavItem[] = [
   { text: '首页', link: '/' },
   { text: '常见问题', link: '/notes/faq/' },
-  { text: '更新日志', link: '/notes/change/' },
+  { text: '控制台演示', link: 'https://www.bestreven.top/rnacos/' },
 ]
